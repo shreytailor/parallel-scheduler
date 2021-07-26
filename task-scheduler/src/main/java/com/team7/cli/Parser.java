afa72a1dee748305c1ecec03f6d83d7b31e9ea6c
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2021-07-26 17:05:41 +1200

    [Feature] Implemented command line parsing.

--- task-scheduler/src/main/java/com/team7/cli/Parser.java
+++ task-scheduler/src/main/java/com/team7/cli/Parser.java
@@ -1,16 +1,51 @@
 package com.team7.cli;
 
+import org.apache.commons.cli.*;
+
 public class Parser {
 
     /**
      *
      * @param args requires commandline arguments to be passed in
-     * @return
+     * @return Config object which embeds all the command line configuration.
      */
-    public static Config parseCommandLineArguments(String[] args){
-        
+    public static Config parseCommandLineArguments(String[] args) {
+        Config config = new Config();
 
-        return null;
+        config.setInputName(args[0]);
+        config.setNumOfProcessors(Integer.parseInt(args[1]));
+
+        Options options = new Options();
+        options.addOption("p", "processors", true, "cores for execution in parallel (default is sequential).");
+        options.addOption("v", "visualisation", false, "visualisation for the algorithm.");
+        options.addOption("o", "output", true, "name for the output file.");
+
+        CommandLine commandLine = null;
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            commandLine = parser.parse(options, args);
+            config.setVisualised(commandLine.hasOption("visualisation"));
+
+            String numOfCores = commandLine.getOptionValue("processors");
+            if (numOfCores != null) {
+                config.setNumOfCores(Integer.parseInt(numOfCores));
+            }
+
+            String outputName = commandLine.getOptionValue("output");
+            if (outputName == null) {
+                String[] fileSegments = config.getInputName().split("\\.(?=[^\\.]+$)");
+                outputName = fileSegments[0] + "-output." + fileSegments[1];
+                config.setOutputName(outputName);
+            } else {
+                config.setOutputName(outputName + ".dot");
+            }
+
+        } catch (ParseException exception) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("Command Line Parameters", options);
+        }
+
+        return config;
     }
-
 }
