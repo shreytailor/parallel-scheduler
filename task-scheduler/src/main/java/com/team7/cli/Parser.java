3163cb43f6917b8e3f43d677a84ecee79de91914
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2021-07-26 17:14:19 +1200

    [Docs] Added documentation for the Parser class.

--- task-scheduler/src/main/java/com/team7/cli/Parser.java
+++ task-scheduler/src/main/java/com/team7/cli/Parser.java
@@ -15,6 +15,7 @@ public class Parser {
         config.setInputName(args[0]);
         config.setNumOfProcessors(Integer.parseInt(args[1]));
 
+        // Informing the CLI framework about possible optional parameters which users can enter.
         Options options = new Options();
         options.addOption("p", "processors", true, "cores for execution in parallel (default is sequential).");
         options.addOption("v", "visualisation", false, "visualisation for the algorithm.");
@@ -27,6 +28,7 @@ public class Parser {
             commandLine = parser.parse(options, args);
             config.setVisualised(commandLine.hasOption("visualisation"));
 
+            // Error handling for the processors, in the case where user doesn't specify a number.
             String numOfCores = commandLine.getOptionValue("processors");
             if (numOfCores != null) {
                 config.setNumOfCores(Integer.parseInt(numOfCores));
@@ -34,14 +36,20 @@ public class Parser {
 
             String outputName = commandLine.getOptionValue("output");
             if (outputName == null) {
+
+                // Using the input name and appending '-output', if output path isn't specified.
                 String[] fileSegments = config.getInputName().split("\\.(?=[^\\.]+$)");
                 outputName = fileSegments[0] + "-output." + fileSegments[1];
                 config.setOutputName(outputName);
             } else {
+
+                // Using the output name, as specified by the user.
                 config.setOutputName(outputName + ".dot");
             }
 
         } catch (ParseException exception) {
+
+            // If there are any exceptions, show the documentation to the user.
             HelpFormatter helpFormatter = new HelpFormatter();
             helpFormatter.printHelp("Command Line Parameters", options);
         }
