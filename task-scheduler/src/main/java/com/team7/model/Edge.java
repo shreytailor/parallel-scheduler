c105b003c0b53f438372b5e231cdaf1ac9e16d55
Oscar <lioscar79@gmail.com>
2021-07-27 12:15:01 +1200

    Create graph modelling classes

---
+++ task-scheduler/src/main/java/com/team7/model/Edge.java
@@ -0,0 +1,7 @@
+package com.team7.model;
+
+public class Edge {
+    private Node head;
+    private Node tail;
+    private int edgeWeight;
+}
---
+++ task-scheduler/src/main/java/com/team7/model/Node.java
@@ -0,0 +1,7 @@
+package com.team7.model;
+
+public class Node {
+    private String name;
+    private int nodeWeight;
+
+}
