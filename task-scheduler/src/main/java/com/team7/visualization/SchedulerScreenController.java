37844a87e489d127a2260de653ee5b8bca96f0d3
simoncheng987 <simoncheng987@gmail.com>
2021-08-18 21:45:25 +1200

    [Feature] Input Graph Resizing feature added.

--- task-scheduler/src/main/java/com/team7/visualization/SchedulerScreenController.java
+++ task-scheduler/src/main/java/com/team7/visualization/SchedulerScreenController.java
@@ -40,12 +40,20 @@ public class SchedulerScreenController implements Initializable {
 
     private final Image SUN_IMAGE = new Image("/images/sun.png");
     private final Image MOON_IMAGE = new Image("/images/moon.png");
+
     private final Image DARK_MIN_IMAGE = new Image("/images/minimise-dark.png");
     private final Image DARK_CLOSE_IMAGE = new Image("/images/close-dark.png");
+    private final Image DARK_OUT_IMAGE = new Image("/images/zoom-out-dark.png");
+    private final Image DARK_IN_IMAGE = new Image("/images/zoom-in-dark.png");
+
     private final Image LIGHT_MIN_IMAGE = new Image("/images/minimise-light.png");
     private final Image LIGHT_CLOSE_IMAGE = new Image("/images/close-light.png");
+    private final Image LIGHT_OUT_IMAGE = new Image("/images/zoom-out-light.png");
+    private final Image LIGHT_IN_IMAGE = new Image("/images/zoom-in-light.png");
+
     private final String LightCss = "/stylesheets/SplashLightMode.css";
     private final String DarkCss = "/stylesheets/SplashDarkMode.css";
+
     private boolean isLightMode = true;
     private boolean isShowingUtilization = true;
 
@@ -57,6 +65,21 @@ public class SchedulerScreenController implements Initializable {
     private ImageView inputGraphDark;
     private BorderPane inputGraphContainer = new BorderPane();
 
+    // Input Graph
+    private BufferedImage lightBufferedImage;
+    private BufferedImage darkBufferedImage;
+
+    private MutableGraph lightMutableGraph;
+    private MutableGraph darkMutableGraph;
+
+    private final int INPUT_GRAPH_MAX_HEIGHT = 650;
+    private final int INPUT_GRAPH_MAX_WIDTH = 500;
+    private final int INPUT_GRAPH_MIN_HEIGHT = 50;
+    private final int INPUT_GRAPH_MIN_WIDTH = 50;
+    private int inputGraphHeight;
+    private int inputGraphWidth;
+    private double HeightAdjustmentRatio; //ratio value that used to resize the input graph
+
     public SchedulerScreenController(Task[] tasks, Schedule schedule, Config config) {
         _config = config;
         _schedule = schedule;
@@ -64,19 +87,28 @@ public class SchedulerScreenController implements Initializable {
 
         try (InputStream dot = new FileInputStream(_config.getInputName())) {
             // parse the dot file and generate an image
-            MutableGraph lg = new Parser().read(dot);
-            lg.graphAttrs().add(Color.TRANSPARENT.background());
-            MutableGraph dg = lg.copy();
-            dg.linkAttrs().add(Color.WHITE);
-            dg.nodeAttrs().add(Color.WHITE);
-            dg.nodeAttrs().add(Color.WHITE.font());
+            lightMutableGraph = new Parser().read(dot);
+            lightMutableGraph.graphAttrs().add(Color.TRANSPARENT.background());
+            darkMutableGraph = lightMutableGraph.copy();
+            darkMutableGraph.linkAttrs().add(Color.WHITE);
+            darkMutableGraph.nodeAttrs().add(Color.WHITE);
+            darkMutableGraph.nodeAttrs().add(Color.WHITE.font());
 
-            BufferedImage imBufferLight = Graphviz.fromGraph(lg).height(650).width(500).render(Format.SVG).toImage();
-            BufferedImage imBufferDark = Graphviz.fromGraph(dg).height(650).width(500).render(Format.SVG).toImage();
+
+            lightBufferedImage = Graphviz.fromGraph(lightMutableGraph).render(Format.SVG).toImage();
+            darkBufferedImage = Graphviz.fromGraph(darkMutableGraph).render(Format.SVG).toImage();
+
+            //DEBUG
+            inputGraphHeight = lightBufferedImage.getHeight();
+            inputGraphWidth = lightBufferedImage.getWidth();
+
+
+            //use double conversion here to calculate the ratio
+            HeightAdjustmentRatio = (double) inputGraphHeight / (double) inputGraphWidth;
 
             // convert the image to javafx component
-            inputGraphLight = new ImageView(SwingFXUtils.toFXImage(imBufferLight, null));
-            inputGraphDark = new ImageView(SwingFXUtils.toFXImage(imBufferDark, null));
+            inputGraphLight = new ImageView(SwingFXUtils.toFXImage(lightBufferedImage, null));
+            inputGraphDark = new ImageView(SwingFXUtils.toFXImage(darkBufferedImage, null));
             inputGraphContainer.setCenter(inputGraphLight);
         } catch (FileNotFoundException e) {
             viewToggleButton.setDisable(true);
@@ -98,6 +130,12 @@ public class SchedulerScreenController implements Initializable {
 
     @FXML
     private Label timerLabel;
+
+    @FXML
+    public ImageView zoomInIcon;
+
+    @FXML
+    public ImageView zoomOutIcon;
 
     @FXML
     public Button viewToggleButton;
@@ -162,8 +200,10 @@ public class SchedulerScreenController implements Initializable {
     @FXML
     private void handleToggleTheme() {
         ObservableList<String> sheets = themeToggleIcon.getScene().getRoot().getStylesheets();
-        
+
         if (isLightMode) {
+            zoomInIcon.setImage(DARK_IN_IMAGE);
+            zoomOutIcon.setImage(DARK_OUT_IMAGE);
             themeToggleIcon.setImage(SUN_IMAGE);
             closeIcon.setImage(DARK_CLOSE_IMAGE);
             minimizeIcon.setImage(DARK_MIN_IMAGE);
@@ -173,8 +213,9 @@ public class SchedulerScreenController implements Initializable {
             sheets.add(DarkCss);
 
             isLightMode = !isLightMode;
-        }
-        else {
+        } else {
+            zoomInIcon.setImage(LIGHT_IN_IMAGE);
+            zoomOutIcon.setImage(LIGHT_OUT_IMAGE);
             themeToggleIcon.setImage(MOON_IMAGE);
             closeIcon.setImage(LIGHT_CLOSE_IMAGE);
             minimizeIcon.setImage(LIGHT_MIN_IMAGE);
@@ -205,13 +246,64 @@ public class SchedulerScreenController implements Initializable {
             inputGraphContainer.setVisible(true);
             viewToggleButton.setText("Show Utilization");
             isShowingUtilization = !isShowingUtilization;
-        }
-        else {
+        } else {
             inputGraphContainer.setVisible(false);
             utilGraphContainer.setVisible(true);
             viewToggleButton.setText("Show Input Graph");
             isShowingUtilization = !isShowingUtilization;
         }
+    }
+
+    @FXML
+    public void handleZoomOutIcon() {
+        //DEBUG
+        System.out.println(inputGraphWidth);
+        System.out.println(inputGraphHeight);
+
+        // When the image size is smaller than the minimum width, return to save overhead
+        if (inputGraphHeight <= INPUT_GRAPH_MIN_HEIGHT || inputGraphWidth <= INPUT_GRAPH_MIN_WIDTH) {
+            return;
+        }
+
+        inputGraphHeight -= 20 * HeightAdjustmentRatio;
+        inputGraphWidth -= 20;
+
+        if (isLightMode) {
+            lightBufferedImage = lightBufferedImage = Graphviz.fromGraph(lightMutableGraph).height(inputGraphHeight).width(inputGraphWidth).render(Format.SVG).toImage();
+            inputGraphLight = new ImageView(SwingFXUtils.toFXImage(lightBufferedImage, null));
+            inputGraphContainer.setCenter(inputGraphLight);
+        } else {
+            darkBufferedImage = darkBufferedImage = Graphviz.fromGraph(darkMutableGraph).height(inputGraphHeight).width(inputGraphWidth).render(Format.SVG).toImage();
+            inputGraphDark = new ImageView(SwingFXUtils.toFXImage(darkBufferedImage, null));
+            inputGraphContainer.setCenter(inputGraphDark);
+        }
+
+    }
+
+    @FXML
+    public void handleZoomInIcon() {
+        //DEBUG
+        System.out.println(inputGraphWidth);
+        System.out.println(inputGraphHeight);
+
+        // When the image size is exceeding the minimum height, return to save overhead
+        if (inputGraphHeight >= INPUT_GRAPH_MAX_HEIGHT || inputGraphWidth >= INPUT_GRAPH_MAX_WIDTH) {
+            return;
+        }
+
+        inputGraphHeight += 20 * HeightAdjustmentRatio;
+        inputGraphWidth += 20;
+
+        if (isLightMode) {
+            lightBufferedImage = lightBufferedImage = Graphviz.fromGraph(lightMutableGraph).height(inputGraphHeight).width(inputGraphWidth).render(Format.SVG).toImage();
+            inputGraphLight = new ImageView(SwingFXUtils.toFXImage(lightBufferedImage, null));
+            inputGraphContainer.setCenter(inputGraphLight);
+        } else {
+            darkBufferedImage = darkBufferedImage = Graphviz.fromGraph(darkMutableGraph).height(inputGraphHeight).width(inputGraphWidth).render(Format.SVG).toImage();
+            inputGraphDark = new ImageView(SwingFXUtils.toFXImage(darkBufferedImage, null));
+            inputGraphContainer.setCenter(inputGraphDark);
+        }
+
     }
 }
 
