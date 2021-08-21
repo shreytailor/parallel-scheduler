8d386d39fca7efb56659d91d3b885a61a77a7d85
Oscar <lioscar79@gmail.com>
2021-08-21 22:36:43 +1200

    [Feature] Update Entrypoint to use the parallel scheduler if the config specifies more than 2 cores

--- task-scheduler/src/main/java/com/team7/Entrypoint.java
+++ task-scheduler/src/main/java/com/team7/Entrypoint.java
@@ -16,11 +16,13 @@ import com.team7.visualization.system.TimeProvider;
 import javafx.application.Platform;
 
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 
 public class Entrypoint {
     public static boolean IS_TEST_MODE = false;
     public static Config config;
     public static Graph graph;
+
     public static void main(String[] args) {
         try {
             // Getting the configuration from the command line, and reading the input graph.
@@ -28,39 +30,46 @@ public class Entrypoint {
             graph = DOTParser.read(config.getInputName());
 
             // Processing the input graph by using the scheduler, and storing the texoutput.
-            Scheduler scheduler = new ParallelScheduler(graph, config.getNumOfProcessors(), config.getNumOfThreads());
+            Scheduler scheduler;
+            if (config.getNumOfThreads() > 1) {
+                scheduler = new ParallelScheduler(graph, config.getNumOfProcessors(), config.getNumOfThreads());
+            } else {
+                scheduler = new Scheduler(graph, config.getNumOfProcessors());
+            }
+
             ScheduleUpdater scheduleUpdater = ScheduleUpdater.getInstance();
             scheduleUpdater.setScheduler(scheduler);
 
             if (config.isVisualised()) {
                 beginVisualisation(scheduler.getTasks(), config, scheduler);
-            }else{
+            } else {
                 long start = System.currentTimeMillis();
-                System.out.println("Start");
+                System.out.println("Started");
                 Schedule schedule = scheduler.findOptimalSchedule();
                 System.out.println("Finished");
                 long finish = System.currentTimeMillis();
-                System.out.println("Time taken: "+(finish-start)+"ms");
+                System.out.println("Time taken: " + (finish - start) + "ms");
+                System.out.println("Optimal schedule generated with makespan of " + schedule.getEstimatedFinishTime());
                 Entrypoint.writeScheduleOutputToFile(schedule);
+                System.exit(0);
             }
-
         } catch (CommandLineException | FileNotFoundException exception) {
             System.out.println(exception.getMessage());
             System.exit(1);
         }
     }
 
-    public static void writeScheduleOutputToFile(Schedule schedule){
-        DOTParser.write(config.getOutputName(),schedule, graph);
+    public static void writeScheduleOutputToFile(Schedule schedule) {
+        DOTParser.write(config.getOutputName(), schedule, graph);
     }
 
-    public static void stopTimerLabel(){
+    public static void stopTimerLabel() {
         if (config != null) {
             if (config.isVisualised()) {
                 TimeProvider.getInstance().stopTimers();
                 VisualizationDriver.updateScreen();
             }
-        }else{
+        } else {
             if (!IS_TEST_MODE) {
                 throw new RuntimeException("Config should have been initialised");
             }
